ab7fe4186b6e8c71510739f871e7aea6db2122e9
Md. Kamran Alam <100942803+mdkamranalam@users.noreply.github.com>
2023-09-13 10:15:58 +0000

    data types

---
+++ 01_basics/02_dataTyppes.js
@@ -0,0 +1,21 @@
+"use stricy"; // treat all JS code as newer version
+
+let name = "Kamran";
+let age = 21;
+let isProgrammer = true;
+let learning = null;
+let address;
+
+// PRIMITIVE DATATYPES
+// Number => 2 to power 53
+// BigInt 
+// String => ""
+// Boolean => true/false
+// null => standalone value (it is an object)
+// undefined => 
+// Symbol => unique 
+
+// NON-PRIMITIVE DATATYPES
+// Object
+
+console.log(typeof isProgrammer);
