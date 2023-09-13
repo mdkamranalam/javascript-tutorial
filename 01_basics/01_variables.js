b983d95725f5c5c3b732fd577d1674bce92d0f08
Md. Kamran Alam <100942803+mdkamranalam@users.noreply.github.com>
2023-09-13 10:15:38 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountID = 123456780;
+const accountName = "Kamran";
+let phoneNum = 8789290747;
+let accountCity = "Patna";
+
+console.table([accountID, accountName, phoneNum, accountCity]);
